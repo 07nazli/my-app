9f5e3c0564458e3921891496ab7c1e31092ab382
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-02-07 20:28:59 +0300

    props

---
+++ src/components/User.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function User(props) {
+  console.log(props);
+  return (
+    <div>
+      USER
+      <div>name: {props.name}</div>
+      <div>age: {props.age}</div>
+      <hr />
+    </div>
+  );
+}
+
+export default User;
+
+// <hr /> divler arasi çizgi koyar
