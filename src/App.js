c622d1fd3f598de807e790ee519b5ab2017144d3
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-02-15 18:24:07 +0300

    new project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import "./App.css";
-import { useState } from "react";
+import "./styles.css";
 
 function App() {
-  const [name, setName] = useState("mario");
-
-  const handleClick = () => {
-    setName("harry");
-    console.log(name);
-  };
-
   return (
-    <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change name</button>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">123,234 * </div>
+        <div className="current-operand">123,234</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
     </div>
   );
 }
